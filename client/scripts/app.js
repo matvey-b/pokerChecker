00fd6c1f6da74039defe103d1e8013cf5990dc48
Matvey Bogomolov <matvey.bogomolov@gmail.com>
2017-05-17 22:51:17 +0700

    Client app.js: Added random hand generator

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,22 +1,46 @@
 var main = function () {
 	console.log("App is working!!!");
-	console.log(getRandomCard());
+	console.log(getRandomHand());
 };
 
-const possibleRanksArray = ["2", "3", "4", "5", "6",
-"7", "8", "9", "10", "jack", "queen", "king", "ace"];
-const possibleSuitsArray = ["hearts", "diamonds", "spades", "clubs"];
+function getRandomHand() {
+	var result = [];
 
-function getRandomInt(min, max) {
-	return Math.floor(Math.random() * (max - min)) + min;
-}
+	const possibleRanks = ["2", "3", "4", "5", "6",
+	"7", "8", "9", "10", "jack", "queen", "king", "ace"];
+	const possibleSuits = ["hearts", "diamonds", "spades", "clubs"];
 
-function getRandomCard() {
-	var result = {};
-	result.rank = possibleRanksArray[getRandomInt(0, 
-		possibleRanksArray.length)];
-	result.suit = possibleSuitsArray[getRandomInt(0,
-		possibleSuitsArray.length)];
+	function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min)) + min;
+	}
+
+	function getRandomCard() {
+		var result = {};
+		result.rank = possibleRanks[getRandomInt(0, possibleRanks.length)];
+		result.suit = possibleSuits[getRandomInt(0, possibleSuits.length)];
+		return result;
+	}
+
+	function iter(generatedCard) {
+		function isEqualToGeneratedCard(card) {
+			const hasEqualSuit = card.suit === generatedCard.suit ?
+			true : false;
+			const hasEqualRank = card.rank === generatedCard.rank ?
+			true : false;
+			return hasEqualSuit && hasEqualRank;
+		}
+		if (result.length < 5) {
+			const countOfEqualToGeneratedCard =
+				result.filter(isEqualToGeneratedCard).length;
+			if (countOfEqualToGeneratedCard === 0) {
+				result.push(generatedCard);
+				iter(getRandomCard());
+			} else {
+				iter(getRandomCard());
+			}
+		}
+	}
+	iter(getRandomCard());
 	return result;
 }
 
