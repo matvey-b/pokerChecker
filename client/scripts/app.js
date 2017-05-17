d60aeae496cf5bb5096dc3c0a91eab92da3c4939
Matvey Bogomolov <matvey.bogomolov@gmail.com>
2017-05-17 08:54:27 +0700

    client app.js: Added random card generator

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,23 @@
 var main = function () {
 	console.log("App is working!!!");
+	console.log(getRandomCard());
 };
+
+const possibleRanksArray = ["2", "3", "4", "5", "6",
+"7", "8", "9", "10", "jack", "queen", "king", "ace"];
+const possibleSuitsArray = ["hearts", "diamonds", "spades", "clubs"];
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomCard() {
+	var result = {};
+	result.rank = possibleRanksArray[getRandomInt(0, 
+		possibleRanksArray.length)];
+	result.suit = possibleSuitsArray[getRandomInt(0,
+		possibleSuitsArray.length)];
+	return result;
+}
 
 $(document).ready(main);
